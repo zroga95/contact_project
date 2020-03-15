37de6eb2f1a561816e0d51cc67458ace0b488fb7
Zachary Roga <zroga95@gmail.com>
2020-03-15 18:23:48 -0400

    updated vcf creation to include name selection

--- createVcfFile.py
+++ createVcfFile.py
@@ -4,7 +4,7 @@ import os
 import json
 import sys
 import re
-
+#names lists retreived from https://github.com/smashew/NameDatabases
 def findPhoneNum(contact_info):
 	for strs in contact_info:
 		match = re.search(r'[0-9]*[.]*[0-9]{3}[.]*[0-9]{4}', strs)
@@ -23,27 +23,45 @@ def findEmail(contact_info):
 			match = ""
 	return match
 
-text_src= r"C:\Users\Zachary_Roga\Documents\contactProject\contact_file2.csv";
-contact_destin = r"C:\Users\Zachary_Roga\Documents\contactProject\contact_sample.vcf";
+
+def findFirstLast(contact_info):
+	first_name_database=base_path + r"\first_names.txt"
+	last_name_database=base_path + r"\last_names.txt"
+	possible_options = []
+	first, last  = "", ""
+	for strs in contact_info:
+		match = re.search(r'[a-zA-Z]{3}[a-zA-Z]*', strs)
+		if match:
+			possible_options.append(match.group())
+	for line in open(first_name_database, "r"):	
+		if line.strip() in possible_options:
+			first=line
+	for line in open(last_name_database, "r"):	
+		if line.strip() in possible_options:
+			last=line
+	return first, last
+
+base_path = r"C:\Users\Zachary_Roga\Documents\contactProject"
+text_src= base_path+r"\contact_file2.csv"
+contact_destin = base_path+ r"\contact_sample.vcf"
 with open(text_src, "r") as contact_file:
-	csv_reader = csv.reader(contact_file, delimiter=',');
+	csv_reader = csv.reader(contact_file, delimiter=',')
 	contact_info = []; #file structure to hold FN, LN, TELEPHONE, EMAIL;)
 	for row in csv_reader:
 		for i in range(1, len(row), 2):
-			contact_info.append(row[i]);
-print(contact_info);
+			contact_info.append(row[i])
+first_name, last_name = findFirstLast(contact_info)
+MyContact = ContactClass(first_name, last_name, findPhoneNum(contact_info), findEmail(contact_info)) #first, last, phone, email
 
-MyContact = ContactClass(contact_info[0], contact_info[1], findPhoneNum(contact_info), findEmail(contact_info));
+allvcf = open(contact_destin, 'w')
+allvcf.write( 'BEGIN:VCARD' + "\n")
+allvcf.write( 'VERSION:3.0' + "\n")
+allvcf.write( 'N:' + MyContact.first + ';' + MyContact.last + "\n")
+allvcf.write( 'FN:' + MyContact.last + ' ' + MyContact.first + "\n")
+#allvcf.write( 'ORG:' + 'ATI' + "\n")
+allvcf.write( 'TEL;CELL:' + MyContact.tele + "\n")
+allvcf.write( 'EMAIL:' + MyContact.email + "\n")
+allvcf.write( 'END:VCARD' + "\n")
+allvcf.write( "\n")
 
-allvcf = open(contact_destin, 'w');
-allvcf.write( 'BEGIN:VCARD' + "\n");
-allvcf.write( 'VERSION:3.0' + "\n");
-allvcf.write( 'N:' + MyContact.first + ';' + MyContact.last + "\n");
-allvcf.write( 'FN:' + MyContact.last + ' ' + MyContact.first + "\n");
-#allvcf.write( 'ORG:' + 'ATI' + "\n");
-allvcf.write( 'TEL;CELL:' + MyContact.tele + "\n");
-allvcf.write( 'EMAIL:' + MyContact.email + "\n");
-allvcf.write( 'END:VCARD' + "\n");
-allvcf.write( "\n");
-
-contact_file.close();
+contact_file.close()
