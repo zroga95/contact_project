0a03109304b1b8fe5e75a1f5ac5a8a0a2c6603ff
Zachary Roga <zroga95@gmail.com>
2020-03-15 16:37:28 -0400

    detecting email and phone number. able to send contact files out in progress

--- createVcfFile.py
+++ createVcfFile.py
@@ -3,19 +3,38 @@ import csv
 import os
 import json
 import sys
+import re
 
-text_src= "C:\Users\Zachary_Roga\Documents\contactProject\contact_file2.csv";
-contact_destin = "C:\Users\Zachary_Roga\Documents\contactProject\contact_sample.vcf";
+def findPhoneNum(contact_info):
+	for strs in contact_info:
+		match = re.search(r'[0-9]*[.]*[0-9]{3}[.]*[0-9]{4}', strs)
+		if match:
+			return match.group()
+		else:
+			match = ""
+	return match
+
+def findEmail(contact_info):
+	for strs in contact_info:
+		match = re.search(r'[\w.-]+@[\w.-]+', strs)
+		if match:
+			return match.group()
+		else:
+			match = ""
+	return match
+
+text_src= r"C:\Users\Zachary_Roga\Documents\contactProject\contact_file2.csv";
+contact_destin = r"C:\Users\Zachary_Roga\Documents\contactProject\contact_sample.vcf";
 with open(text_src, "r") as contact_file:
 	csv_reader = csv.reader(contact_file, delimiter=',');
 	contact_info = []; #file structure to hold FN, LN, TELEPHONE, EMAIL;)
 	for row in csv_reader:
 		for i in range(1, len(row), 2):
-			if len(contact_info) < 4:
-				contact_info.append(row[i]);
+			contact_info.append(row[i]);
 print(contact_info);
-MyContact = ContactClass(contact_info[0], contact_info[1], contact_info[2], contact_info[3]);
-print(MyContact.last)
+
+MyContact = ContactClass(contact_info[0], contact_info[1], findPhoneNum(contact_info), findEmail(contact_info));
+
 allvcf = open(contact_destin, 'w');
 allvcf.write( 'BEGIN:VCARD' + "\n");
 allvcf.write( 'VERSION:3.0' + "\n");
